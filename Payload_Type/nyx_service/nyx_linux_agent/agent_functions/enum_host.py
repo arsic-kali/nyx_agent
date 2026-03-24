3f7927e820a91970c6600ba348c94db2d7f95855
meisty <shaundixon666@gmail.com>
2026-03-24 19:14:24 -0400

    Updated agent function to handle full and quick modes

--- Payload_Type/nyx_service/nyx_linux_agent/agent_functions/enum_host.py
+++ Payload_Type/nyx_service/nyx_linux_agent/agent_functions/enum_host.py
@@ -1,19 +1,35 @@
-from mythic_container.MythicCommandBase import *
+from mythic_container.MythicCommandBase import (
+    CommandBase,
+    TaskArguments,
+    ParameterType,
+)
 
 class EnumHostArguments(TaskArguments):
-    def __init__(self):
-        super().__init__()
-        self.args = []
+    def __init__(self, command_line, **kwargs):
+        super().__init__(command_line, **kwargs)
+        # Define a single string parameter called "mode"
+        self.args = [
+            {
+                "name": "mode",
+                "type": ParameterType.String,
+                "description": "Enumeration mode: quick (default) or full",
+                "default_value": "quick",
+            }
+        ]
+
+    async def parse_arguments(self):
+        """
+        Parse the input command line from Mythic into parameters
+        """
+        if self.command_line:
+            self.add_arg("mode", self.command_line.strip())
+
 
 class EnumHostCommand(CommandBase):
-    cmd = "enum_host"
+    cmd = "enum_host"  # Command name in Mythic
     needs_admin = False
-    help_cmd = "enum_host"
-    description = "Enumerate host information (system, user, network, processes)"
+    help_cmd = "enum_host [quick|full]"
+    description = "Enumerate host information (system, user, network, files, etc.)"
     version = 1
     author = "@meisty"
-
     argument_class = EnumHostArguments
-
-    async def create_tasking(self, task):
-        return task
